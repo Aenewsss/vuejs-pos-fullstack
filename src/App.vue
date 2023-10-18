27c3ee67e8a56c733d28edcc43dd47e80c80aa07
aenewsss <aenamartinelli@gmail.com>
2023-10-17 23:29:49 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Meu primeiro App Vue.js"/>
+  <NewComponent />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import NewComponent from './components/New.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    NewComponent
   }
 }
 </script>
---
+++ src/components/New.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>New component</h1>
+</template>
+
+<script>
+    export default {
+        name: 'NewComponent'
+    }
+</script>
+
+<style scoped>
+    h1 {
+        color: red;
+    }
+</style>
